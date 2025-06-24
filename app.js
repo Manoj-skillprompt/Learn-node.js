0493d56c4fc62b83dc9e3298d5f808d89440b11b
Manoj <manojtharu.skillpromt23@gmail.com>
2025-06-24 12:14:38 +0545

    global variable

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log(__dirname)
+console.log(__filename)
+
+// define global variables in node js
+global.myVariable='Manoj'
+console.log(myVariable)
+
+// output:
+// D:\SkillPrompt\learn backend\node
+// D:\SkillPrompt\learn backend\node\app.js
+// Manoj
