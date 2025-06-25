078780a4bfc4e563e2e58ee357c845c79df4856e
Manoj <manojtharu.skillpromt23@gmail.com>
2025-06-25 16:30:44 +0545

    added fs.mkdir() and fs.writeFile()

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const sayHello = require("./module");
 const multimodule = require("./multimodule");
 const fun3 = require("./multimodule");
@@ -8,3 +9,39 @@ sayHello('Manoj Kumar Chaudhary')
 multimodule.fun1("Ram")
 multimodule.fun2("Ram")
 multimodule.fun3("Ram")
+
+
+// make a new directory called ./myFolder
+const fs= require('fs')
+fs.mkdir('./myFolder',(err)=>{
+    if(err){
+        console.log('error')
+
+    }else{
+        console.log('directory created successfully')
+    }
+
+})
+
+// create and write to a file asynchronousely
+// the problem is when you run the same code multiple times, it erase previous data that is already present and write the data to it
+fs.writeFile('./myFolder/newFile.txt','This is the example of creating new file and write data to it',(err)=>{
+    if(err){
+        console.log(err)
+        return
+    }else{
+        console.log('File created successfully')
+    }
+})
+
+
+// the solution is just add third parameter {flag:'a'}, this will append new data
+
+fs.writeFile('./myFolder/newnewFile.txt','Successfully done',{flag:'a'},(err)=>{
+    if(err){
+        console.log(err)
+        return;
+    }else{
+        console.log('successfully done')
+    }
+})
